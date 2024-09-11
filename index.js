bc1f3247110361f9e0631cbd24e2208279ef578e
Ryann <ryanncarlos.247@gmail.com>
2024-09-10 22:00:15 -0300

    fundamentos 004

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-// arrays , objetos
- let meta = {
-    value: 'ler um livro por mes',
-    checked: true,
- }
-
- let metas = [
-    meta,
-    {
-    value: 'caminhar 20 minutos',
-    checked: false
+function start() {
+    let count = 0
+    while(count <= 10){
+        console.log(count)
+        count++
+    }
 }
-]
 
- console.log(metas[1].value)
+start()
